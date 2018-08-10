807bbce61ccd9013fe03cfde7c2e8d9f09bac8fd
Prakash Pandey <pcpandey@mail.com>
2018-08-10 17:53:33 +0530

    Adding utility file. This will contain various utilities used in the application

---
+++ src/utils.py
@@ -0,0 +1,26 @@
+# Project utility functions
+
+def find_longest_sub_list(list_of_lists):
+    """
+        Find the list having largest length
+    """
+    largest = []
+    if list_of_lists:
+        largest = list_of_lists[0]
+        for list in list_of_lists[1:]:
+            if len(list) > len(largest):
+                largest = list
+    
+    return largest
+
+
+
+if __name__ == "__main__":
+    list = [
+        ['A'],
+         ['A', 'B', 'C', 'E'],
+        ['A', 'B'],
+        ['A', 'B', 'C']
+    ]
+    # list = []
+    print(find_longest_sub_list(list))
