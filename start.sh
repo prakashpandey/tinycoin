199e19c1eb9ac220d6f272af08cdb9f302bb76d8
Prakash Pandey <pcpandey@mail.com>
2018-08-21 18:46:11 +0530

    Added script which exports environment vars and start application

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# Define environment variables
+HOST="0.0.0.0"
+PORT=5000
+PEERS="192.168.1.11,192.168.1.12"
+
+# Export environment variables
+echo "Exporting HOST=$HOST"
+export HOST=$HOST
+echo "Exporting PORT=$PORT"
+export PORT=$PORT
+echo "Exporting PEERS=$PEERS"
+export PEERS=$PEERS
+
+# Start application
+echo "Starting application.... "
 python src/app.py
