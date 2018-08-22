577f54948d8deadafd5cad0bc29a0fae2d1cd85f
Prakash Pandey <pcpandey@mail.com>
2018-08-22 14:05:15 +0530

    Added mining address in start script

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@
 HOST="0.0.0.0"
 PORT=5000
 PEERS="192.168.1.11:5000,192.168.1.12:5000"
+MINER_ADDRESS="ppdpp-dvfgf-fredgdsdf-gdsfgsd-35vr433-ee2eass4d"
 
 # Export environment variables
 echo "Exporting HOST=$HOST"
@@ -12,6 +13,8 @@ echo "Exporting PORT=$PORT"
 export PORT=$PORT
 echo "Exporting PEERS=$PEERS"
 export PEERS=$PEERS
+echo "Exporting MINER_ADDRESS=$MINER_ADDRESS"
+export MINER_ADDRESS=$MINER_ADDRESS
 
 # Start application
 echo "Starting application.... "
