4dd2f0b4010a151cbe7faf9a6b01a0dc3b94d68d
Prakash Pandey <pcpandey@mail.com>
2018-08-23 04:12:25 +0530

    Added http host

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define environment variables
-HOST="127.0.0.1"
+HOST="http://127.0.0.1"
 PORT=5000
 PEERS="http://127.0.0.1:5001"
 MINER_ADDRESS="ppdpp-dvfgf-fredgdsdf-gdsfgsd-35vr433-ee2eass4d"
